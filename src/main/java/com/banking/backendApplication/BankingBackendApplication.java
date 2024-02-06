b9588f57cd9390465997be794e29d8b2e21ae5d9
shivam.vashishtha <shivam.vashishtha@shl.com>
2024-02-06 18:34:22 +0530

    method level security

--- src/main/java/com/banking/backendApplication/BankingBackendApplication.java
+++ src/main/java/com/banking/backendApplication/BankingBackendApplication.java
@@ -3,8 +3,10 @@ package com.banking.backendApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class BankingBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/banking/backendApplication/controller/ContactController.java
+++ src/main/java/com/banking/backendApplication/controller/ContactController.java
@@ -3,11 +3,14 @@ package com.banking.backendApplication.controller;
 import com.banking.backendApplication.model.Contact;
 import com.banking.backendApplication.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -17,7 +20,10 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+//    @PreFilter("filterObject.contactName != 'Test'")
+    @PostFilter("filterObject.contactName != 'Test'")
+    public Contact saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
         return contactRepository.save(contact);
