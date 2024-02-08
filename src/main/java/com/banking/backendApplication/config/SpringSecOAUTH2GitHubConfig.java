3745ae2c5692280306af5b2ca6f0de7a0c27a8e9
11shivam23 <11shivam23@gmail.com>
2024-02-09 01:36:41 +0530

    OAUTH 2

---
+++ src/main/java/com/banking/backendApplication/config/SpringSecOAUTH2GitHubConfig.java
@@ -0,0 +1,31 @@
+package com.banking.backendApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTH2GitHubConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login();
+        return http.build();
+    }
+
+//    @Bean
+//    public ClientRegistrationRepository clientRepository() {
+//        ClientRegistration clientReg = clientRegistration();
+//        return new InMemoryClientRegistrationRepository(clientReg);
+//    }
+//
+//    private ClientRegistration clientRegistration() {
+//        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("dacbc905d88ff4ce81b3")
+//                .clientSecret("6d018193a4b22f08c074cb690388d8d5868334cc").build();
+//    }
+}
--- src/test/java/com/banking/backendApplication/SpringSecurityBasicApplicationTests.java
+++ src/test/java/com/banking/backendApplication/SpringSecurityBasicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springSecurityBasic;
+package com.banking.backendApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
